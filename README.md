5e8e2295af3951b424d1276bd9edaa88c8f3ef61
Matej Kovac <kovach.matej@gmail.com>
2018-09-08 18:31:57 +0200

    React Calendar Final Code

---
+++ README.md
@@ -0,0 +1,14 @@
+# React calendar component
+
+![Custom React Calendar Component](./screen.png "Custom React Calendar Component")
+
+[Read the blog](https://blog.flowandform.agency/create-a-custom-calendar-in-react-3df1bfd0b728) about how to create and expand custom React calendar component
+
+To start it in your environment, clone the repo and while in folder, execute following commands
+
+~~~
+npm install
+npm run start
+~~~
+
+More at [Moodydev.io](http://moodydev.io "Moodydev")
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Calendar from "./components/Calendar";
+
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <div id="logo">
+            <span className="icon">date_range</span>
+            <span>
+              react<b>calendar</b>
+            </span>
+          </div>
+        </header>
+        <main>
+          <Calendar />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
+//
